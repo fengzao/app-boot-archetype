d94544c0e19bdc79cd4e668efccbdcb5cf81c0e1
fengzao <zhangming360@gmail.com>
2019-04-22 09:58:50 +0800

    完善 命令说明

--- archetype.sh
+++ archetype.sh
@@ -2,8 +2,9 @@
 
 mvn clean
 
-rm -rf logs
-
+# 使用 archetype:create-from-project 从 Maven 项目中构建 Archetype 项目
+# -Darchetype.properties 用于指定配置文件
+# 关于配置参数见官方文档 http://maven.apache.org/archetype/maven-archetype-plugin/create-from-project-mojo.html
 mvn clean archetype:create-from-project -Darchetype.properties=archetype.properties
 
 rm -rf target/generated-sources/archetype/src/main/resources/archetype-resources/.idea
